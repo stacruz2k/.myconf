0dfdd1423aa28461d7e157b87503223d90cf62e7
Juan SC <jsc@mail.me>
2025-01-06 16:10:51 -0600

    .

--- skls/dwmblocks/blocks.h
+++ skls/dwmblocks/blocks.h
@@ -1,8 +1,8 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"",    "ipnet.sh",    60,     0},
-    {"",    "vol.sh",      10,     0},
+	{"",    "ipnet.sh",    5,     0},
+    {"",    "vol.sh",      5,     0},
     {"",    "mem.sh",      30,     0},
     {"",    "time.sh",     60,		0},
 };
